b39f5f7c8fe9c08ec0968ad41867e53a4be09d71
Joseph Sims <jsims@slower.ai>
2026-01-06 15:20:31 -0700

    Add shoryuken ECS Service in DEV

--- terraform/envs/dev/resque/main.tf
+++ terraform/envs/dev/resque/main.tf
@@ -23,3 +23,15 @@ module "resque-scheduler" {
 
   manage_task_definition = false
 }
+
+module "shoryuken" {
+  source        = "github.com/chanzuckerberg/cztack//aws-ecs-job?ref=v0.41.0"
+  desired_count = 1
+  env           = var.env
+  service       = "shoryuken"
+  project       = var.project
+  owner         = var.owner
+  cluster_id    = data.terraform_remote_state.ecs.outputs.cluster_id
+  task_role_arn = data.terraform_remote_state.web.outputs.task_role_arn
+  manage_task_definition = false
+}
