c196f84e132b0dca2565e9f6120be412c1c6a482
Joseph Sims <jsims@slower.ai>
2026-01-16 15:17:47 -0700

    Update redis version in redis component in staging

--- terraform/envs/staging/redis/main.tf
+++ terraform/envs/staging/redis/main.tf
@@ -1,17 +1,14 @@
 module "elasticache_secure" {
-  # TODO Point to the correct ref version tag once this new module is merged into cztack
   source                        = "github.com/chanzuckerberg/cztack//aws-redis-replication-group?ref=v0.103.2"
-  project                       = var.project
-  env                           = var.env
-  service                       = "resque-secure"
-  owner                         = var.owner
   ingress_security_group_ids    = [data.terraform_remote_state.ecs.outputs.security_group_id]
   subnets                       = data.terraform_remote_state.cloud-env.outputs.private_subnets
-  engine_version                = "5.0.3"
-  parameter_group_name          = "default.redis5.0"
+  engine_version                = "7.1" //Upgraded as v5 is no longer supported
+  parameter_group_name          = "default.redis7"
   instance_type                 = "cache.m5.large"
   at_rest_encryption_enabled    = true
   transit_encryption_enabled    = true
   vpc_id                        = data.terraform_remote_state.cloud-env.outputs.vpc_id
-  replication_group_description = "Secure redis group"
+  description                   = "Secure redis group"
+  tags                          = var.tags
+  auth_token                    = null
 }
