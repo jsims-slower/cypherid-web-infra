348c0ffd6eca29bdd9f2be64f1dbe25d3e6dca2e
Joseph Sims <jsims@slower.ai>
2026-01-19 13:39:39 -0700

    Add shoryuken to resque in staging

--- terraform/envs/staging/resque/main.tf
+++ terraform/envs/staging/resque/main.tf
@@ -1,15 +1,3 @@
-module "resque" {
-  source        = "github.com/chanzuckerberg/cztack//aws-ecs-job?ref=v0.103.2"
-  desired_count = 2
-  env           = var.env
-  service       = "resque"
-  project       = var.project
-  owner         = var.owner
-  cluster_id    = data.terraform_remote_state.ecs.outputs.cluster_id
-  task_role_arn = data.terraform_remote_state.web.outputs.task_role_arn
-
-  manage_task_definition = false
-}
 
 module "resque-pipeline-monitor" {
   source        = "github.com/chanzuckerberg/cztack//aws-ecs-job?ref=v0.103.2"
@@ -47,5 +35,17 @@ module "resque-scheduler" {
   cluster_id    = data.terraform_remote_state.ecs.outputs.cluster_id
   task_role_arn = data.terraform_remote_state.web.outputs.task_role_arn
 
+  manage_task_definition = false
+}
+
+module "shoryuken" {
+  source        = "github.com/chanzuckerberg/cztack//aws-ecs-job?ref=v0.103.2"
+  desired_count = 1
+  env           = var.env
+  service       = "shoryuken"
+  project       = var.project
+  owner         = var.owner
+  cluster_id    = data.terraform_remote_state.ecs.outputs.cluster_id
+  task_role_arn = data.terraform_remote_state.web.outputs.task_role_arn
   manage_task_definition = false
 }
