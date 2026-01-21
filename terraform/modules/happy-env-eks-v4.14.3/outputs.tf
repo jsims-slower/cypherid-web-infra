4f4510657d9211e3d4dd6b77c7f7925bc33fe725
Joseph Sims <jsims@slower.ai>
2026-01-21 13:35:45 -0700

    Bump happy-env-eks to v4.14.3

---
+++ terraform/modules/happy-env-eks-v4.14.3/outputs.tf
@@ -0,0 +1,26 @@
+output "namespace" {
+  value = local.k8s_namespace
+}
+
+
+output "dashboard" {
+  value = {
+    id  = datadog_dashboard_json.environment_dashboard.id
+    url = datadog_dashboard_json.environment_dashboard.url
+  }
+}
+
+output "integration_secret" {
+  value     = local.secret_string
+  sensitive = true
+}
+
+output "databases" {
+  value = { for k, v in module.dbs : k => {
+    database_host     = v.endpoint
+    database_name     = v.database_name
+    database_username = v.master_username
+    database_password = v.master_password
+  } }
+  sensitive = true
+}
