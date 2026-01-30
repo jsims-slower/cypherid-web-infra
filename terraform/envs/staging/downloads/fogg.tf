eaad4807708e97ce1f838fcea9e715486638d2fa
Joseph Sims <jsims@slower.ai>
2026-01-30 12:35:17 -0700

    Enable downloads component in staging

--- terraform/envs/staging/downloads/fogg.tf
+++ terraform/envs/staging/downloads/fogg.tf
@@ -155,7 +155,28 @@ terraform {
     aws = {
       source = "hashicorp/aws"
 
-      version = "5.94.0"
+      version = "5.100.0"
+
+    }
+
+    helm = {
+      source = "hashicorp/helm"
+
+      version = "2.17.0"
+
+    }
+
+    kubectl = {
+      source = "gavinbunney/kubectl"
+
+      version = "1.19.0"
+
+    }
+
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+
+      version = "3.0.1"
 
     }
 
