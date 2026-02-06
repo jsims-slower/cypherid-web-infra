285080cdfd10c4fa6212ad76122284edb4c12fa8
Joseph Sims <jsims@slower.ai>
2026-02-06 12:14:14 -0700

    Rename module from k8s-core-v5.5.1 to k8s-core-k8s-core-v5

--- terraform/modules/k8s-core-k8s-core-v5/outputs.tf
+++ terraform/modules/k8s-core-k8s-core-v5/outputs.tf
@@ -3,9 +3,9 @@ output "default_namespace" {
   value       = kubernetes_namespace.default_namespace.metadata[0].name
 }
 
-# output "aws_ssm_iam_role_name" {
-#   value = module.aws-ssm.aws_ssm_iam_role_name
-# }
+output "aws_ssm_iam_role_name" {
+  value = module.aws-ssm.aws_ssm_iam_role_name
+}
 
 output "datadog_agent_hostname" {
   value = "DEPRECATED"
