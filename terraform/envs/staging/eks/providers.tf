6f01bf54df14f932843e8e1dfe937c62ac402e33
Joseph Sims <jsims@slower.ai>
2026-02-09 18:16:59 -0700

    Fix helm provider in eks staging

--- terraform/envs/staging/eks/providers.tf
+++ terraform/envs/staging/eks/providers.tf
@@ -50,10 +50,10 @@ provider "kubectl" {
 }
 
 provider "helm" {
-  kubernetes = {
+  kubernetes {
     host                   = module.eks-cluster.cluster_endpoint
     cluster_ca_certificate = base64decode(module.eks-cluster.cluster_certificate_authority_data)
-    exec = {
+    exec {
       api_version = "client.authentication.k8s.io/v1beta1"
       command     = "aws"
       args        = ["eks", "get-token", "--cluster-name", module.eks-cluster.cluster_id]
