d42cba32cb77831b75d562b8dc1c0c1a8a3cf023
Joseph Sims <jsims@slower.ai>
2026-04-09 15:55:51 -0600

    Remove terraform.lock.hcl files from Git. Add generated files in prod, even though they are not yet complete.

--- terraform/modules/czid-services-private-key/outputs.tf
+++ terraform/modules/czid-services-private-key/outputs.tf
@@ -1,17 +1,14 @@
 output "private_key_pem" {
-  # value     = tls_private_key.ecdsa-p384.private_key_pem
   value     = trimspace(tls_private_key.ecdsa-p384.private_key_pem)
   sensitive = true
 }
 
 output "public_key_pem" {
-  # value     = tls_private_key.ecdsa-p384.private_key_pem
   value     = trimspace(tls_private_key.ecdsa-p384.public_key_pem)
   sensitive = false
 }
 
 output "public_key_openssh" {
-  # value     = tls_private_key.ecdsa-p384.private_key_pem
   value     = trimspace(tls_private_key.ecdsa-p384.public_key_openssh)
   sensitive = false
 }
